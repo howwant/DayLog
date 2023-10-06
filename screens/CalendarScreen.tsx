98f26ac91ca86e6983a3d5b4d1b3faebed429506
hwawon <corn1128@naver.com>
2023-10-06 15:23:39 +0900

    리액트 네이티브 다이어리

---
+++ screens/CalendarScreen.tsx
@@ -0,0 +1,82 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {Animated, Button, StyleSheet, View} from 'react-native';
+
+function SlideLeftAndRight() {
+  const animation = useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 1 : 0,
+      useNativeDriver: true,
+    }).start();
+  }, [enabled, animation]);
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
+    </View>
+  );
+}
+
+function FadeInAndOut() {
+  const animation = useRef(new Animated.Value(1)).current;
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: hidden ? 0 : 1,
+      useNativeDriver: true,
+    }).start();
+  }, [hidden, animation]);
+
+  return (
+    <View>
+      <Animated.View style={[styles.rectangle, {opacity: animation}]} />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setHidden(!hidden);
+        }}
+      />
+    </View>
+  );
+}
+
+function CalendarScreen() {
+  return (
+    <View style={styles.block}>
+      <SlideLeftAndRight />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  block: {},
+  rectangle: {width: 100, height: 100, backgroundColor: 'black'},
+});
+
+export default CalendarScreen;
---
+++ screens/WriteScreen.tsx
@@ -0,0 +1,92 @@
+import React, {useContext, useState} from 'react';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import WriteHeader from './WriteHeader';
+import WriteEditor from '../components/WriteEditor';
+import {useNavigation} from '@react-navigation/native';
+import LogContext from '../contexts/LogContext';
+
+function WriteScreen({route}) {
+  const log = route.params?.log;
+
+  const [title, setTitle] = useState(log?.title ?? '');
+  const [body, setBody] = useState(log?.body ?? '');
+
+  const navigation = useNavigation();
+  const {onCreate, onModify, onRemove} = useContext(LogContext);
+  const onSave = () => {
+    if (log) {
+      onModify({
+        id: log.id,
+        date: log.date,
+        title,
+        body,
+      });
+    } else {
+      onCreate({
+        title,
+        body,
+        date: new Date().toISOString(),
+      });
+    }
+    navigation.pop();
+  };
+  const onAskRemove = () => {
+    Alert.alert(
+      '삭제',
+      '정말로 삭제하시겠어요?',
+      [
+        {
+          text: '취소',
+          style: 'cancel',
+        },
+        {
+          text: '삭제',
+          onPress: () => {
+            onRemove(log?.id);
+            navigation.pop();
+          },
+          style: 'destructive',
+        },
+      ],
+      {cancelable: true},
+    );
+  };
+  return (
+    <SafeAreaView style={styles.block}>
+      <KeyboardAvoidingView
+        style={styles.avoidingView}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <WriteHeader
+          onSave={onSave}
+          onAskRemove={onAskRemove}
+          isEditing={!!log}
+        />
+        <WriteEditor
+          title={title}
+          body={body}
+          onChangeTitle={setTitle}
+          onChangeBody={setBody}
+        />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoidingView: {
+    flex: 1,
+  },
+});
+
+export default WriteScreen;
