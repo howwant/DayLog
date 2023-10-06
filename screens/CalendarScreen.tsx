fca9e219748d64d42fc98e196b8d69ced236e33f
kanghwawon <corn1128@naver.com>
2023-10-06 16:36:42 +0900

    캘린더에서 일정 나오도록 추가

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,82 +1,112 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Animated, Button, StyleSheet, View} from 'react-native';
-
-function SlideLeftAndRight() {
-  const animation = useRef(new Animated.Value(0)).current;
-  const [enabled, setEnabled] = useState(false);
-
-  useEffect(() => {
-    Animated.timing(animation, {
-      toValue: enabled ? 1 : 0,
-      useNativeDriver: true,
-    }).start();
-  }, [enabled, animation]);
-
-  return (
-    <View>
-      <Animated.View
-        style={[
-          styles.rectangle,
-          {
-            transform: [
-              {
-                translateX: animation.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [0, 150],
-                }),
-              },
-            ],
-            opacity: animation.interpolate({
-              inputRange: [0, 1],
-              outputRange: [1, 0],
-            }),
-          },
-        ]}
-      />
-      <Button
-        title="Toggle"
-        onPress={() => {
-          setEnabled(!enabled);
-        }}
-      />
-    </View>
-  );
-}
-
-function FadeInAndOut() {
-  const animation = useRef(new Animated.Value(1)).current;
-  const [hidden, setHidden] = useState(false);
-
-  useEffect(() => {
-    Animated.timing(animation, {
-      toValue: hidden ? 0 : 1,
-      useNativeDriver: true,
-    }).start();
-  }, [hidden, animation]);
-
-  return (
-    <View>
-      <Animated.View style={[styles.rectangle, {opacity: animation}]} />
-      <Button
-        title="Toggle"
-        onPress={() => {
-          setHidden(!hidden);
-        }}
-      />
-    </View>
-  );
-}
+import {format} from 'date-fns';
+import React, {useContext, useMemo, useState} from 'react';
+import CalendarView from '../components/CalendarView';
+import LogContext from '../contexts/LogContext';
+import FeedList from '../components/FeedList';
 
 function CalendarScreen() {
+  const {logs} = useContext(LogContext);
+  const [selectedDate, setSelectedDate] = useState(
+    format(new Date(), 'yyyy-MM-dd'),
+  );
+
+  const markedDates = useMemo(
+    () =>
+      logs.reduce((acc, current) => {
+        const formattedDate = format(new Date(current.date), 'yyyy-MM-dd');
+        acc[formattedDate] = {marked: true};
+        return acc;
+      }, {}),
+    [logs],
+  );
+
+  const filteredLogs = logs.filter(
+    log => format(new Date(log.date), 'yyyy-MM-dd') === selectedDate,
+  );
+
   return (
-    <View style={styles.block}>
-      <SlideLeftAndRight />
-    </View>
+    <FeedList
+      logs={filteredLogs}
+      ListHeaderComponent={
+        <CalendarView
+          markedDates={markedDates}
+          selectedDate={selectedDate}
+          onSelectDate={setSelectedDate}
+        />
+      }
+    />
   );
 }
-const styles = StyleSheet.create({
-  block: {},
-  rectangle: {width: 100, height: 100, backgroundColor: 'black'},
-});
 
 export default CalendarScreen;
+
+// 연습용
+// function SlideLeftAndRight() {
+//   const animation = useRef(new Animated.Value(0)).current;
+//   const [enabled, setEnabled] = useState(false);
+
+//   useEffect(() => {
+//     Animated.timing(animation, {
+//       toValue: enabled ? 1 : 0,
+//       useNativeDriver: true,
+//     }).start();
+//   }, [enabled, animation]);
+
+//   return (
+//     <View>
+//       <Animated.View
+//         style={[
+//           styles.rectangle,
+//           {
+//             transform: [
+//               {
+//                 translateX: animation.interpolate({
+//                   inputRange: [0, 1],
+//                   outputRange: [0, 150],
+//                 }),
+//               },
+//             ],
+//             opacity: animation.interpolate({
+//               inputRange: [0, 1],
+//               outputRange: [1, 0],
+//             }),
+//           },
+//         ]}
+//       />
+//       <Button
+//         title="Toggle"
+//         onPress={() => {
+//           setEnabled(!enabled);
+//         }}
+//       />
+//     </View>
+//   );
+// }
+
+// function FadeInAndOut() {
+//   const animation = useRef(new Animated.Value(1)).current;
+//   const [hidden, setHidden] = useState(false);
+
+//   useEffect(() => {
+//     Animated.timing(animation, {
+//       toValue: hidden ? 0 : 1,
+//       useNativeDriver: true,
+//     }).start();
+//   }, [hidden, animation]);
+
+//   return (
+//     <View>
+//       <Animated.View style={[styles.rectangle, {opacity: animation}]} />
+//       <Button
+//         title="Toggle"
+//         onPress={() => {
+//           setHidden(!hidden);
+//         }}
+//       />
+//     </View>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   block: {},
+//   rectangle: {width: 100, height: 100, backgroundColor: 'black'},
+// });
